5f221596584299d632851892a0396fe485d4c1f9
cringgaard <christoffer@ringgaard.com>
2023-03-29 12:58:36 +0200

    Restructured data and fine tuned ViT

--- data/boats_dataset.py
+++ data/boats_dataset.py
@@ -1,5 +1,7 @@
 import datasets
 from datasets import Dataset, DatasetDict , ClassLabel
+from .classes import *
+import numpy as np
 
 import os
 from datasets.tasks import ImageClassification
@@ -16,8 +18,8 @@ FGVC of boats
 """
 _FEATURES = datasets.Features({
     "img_path" : datasets.Image(),
-    "Hull Type" : ClassLabel(names_file="https://huggingface.co/datasets/cringgaard/boats_dataset/raw/main/labels/Hull_Type.txt"),
-    "Rigging Type" : ClassLabel(names_file="https://huggingface.co/datasets/cringgaard/boats_dataset/raw/main/labels/Rigging_Type.txt"),
+    "Hull Type" : ClassLabel(names = list(Hull_Type_Classes.values())),
+    "Rigging Type" : ClassLabel(names = list(Rigging_Type_Classes.values())),
     # "LOA" : datasets.Value('float64'),
     # "LWL" : datasets.Value('float64'),
     # "Beam" : datasets.Value('float64'),
@@ -28,11 +30,11 @@ _FEATURES = datasets.Features({
     # "S.A./Disp." : datasets.Value('float64'),
     # "Bal./Disp." : datasets.Value('float64'),
     # "Disp./Len." : datasets.Value('float64'),
-    "Construction" : ClassLabel(names_file="https://huggingface.co/datasets/cringgaard/boats_dataset/raw/main/labels/Construction.txt"),
-    "Ballast Type" : ClassLabel(names_file="https://huggingface.co/datasets/cringgaard/boats_dataset/raw/main/labels/Ballast Type.txt"),
+    "Construction" : ClassLabel(names = list(Construction_Classes.values())),
+    "Ballast Type" : ClassLabel(names = list(Ballast_Type_Classes.values())),
     # "First Built" : datasets.Value('float64'),
     # "# Built" : datasets.Value('float64'),
-    "Designer" : ClassLabel(names_file="https://huggingface.co/datasets/cringgaard/boats_dataset/raw/main/labels/Designer.txt"),
+    "Designer" : ClassLabel(names= list(Designer_Classes.values())),
     # "Comfort Ratio" : datasets.Value('float64'),
     # "Capsize Screening Formula" : datasets.Value('float64'),
     # "S#" : datasets.Value('float64'),
@@ -46,7 +48,7 @@ _FEATURES = datasets.Features({
     # "S.A. Main" : datasets.Value('float64'),
     # "S.A./Disp. (calc.)" : datasets.Value('float64'),
     # "Est. Forestay Len." : datasets.Value('float64'),
-    "name" : ClassLabel(names_file="https://huggingface.co/datasets/cringgaard/boats_dataset/raw/main/labels/name.txt"),
+    "name" : ClassLabel(names = list(Name_Classes.values())),
     # "Draft (min)" : datasets.Value('float64'),
     # "Builder" : datasets.Value('float'),
     # "Model" : datasets.Value('float'),
@@ -113,14 +115,21 @@ class BOATS(datasets.GeneratorBasedBuilder):
             datasets.SplitGenerator(
                 name=datasets.Split.TRAIN, 
                 gen_kwargs={
-                    "filepath": "boat_data_train.csv",
+                    "filepath": "https://huggingface.co/datasets/cringgaard/boats_dataset/raw/main/boat_data_train.csv",
                     "images": dl_manager.iter_archive(archive_path),
                 },
             ),
             datasets.SplitGenerator(
                 name=datasets.Split.TEST, 
                 gen_kwargs={
-                    "filepath": "boat_data_test.csv",
+                    "filepath": "https://huggingface.co/datasets/cringgaard/boats_dataset/raw/main/boat_data_test.csv",
+                    "images": dl_manager.iter_archive(archive_path),
+                }
+            ),
+            datasets.SplitGenerator(
+                name=datasets.Split("full"), 
+                gen_kwargs={
+                    "filepath": "https://huggingface.co/datasets/cringgaard/boats_dataset/raw/main/boat_data_clean.csv",
                     "images": dl_manager.iter_archive(archive_path),
                 }
             ),
@@ -131,54 +140,13 @@ class BOATS(datasets.GeneratorBasedBuilder):
         df = pd.read_csv(filepath)
         for file_path, file_obj in images:
             if sum(df['img_path'] == file_path.split("/")[1]) > 0:
-                yield file_path, {
-                    "img_path" : {"path": file_path, "bytes": file_obj.read()},
-                    "Hull Type" : df['Hull Type'][df['img_path'] == file_path.split("/")[1]].values[0],
-                    "Rigging Type" : df['Rigging Type'][df['img_path'] == file_path.split("/")[1]].values[0],
-                    # "LOA" : df['LOA'][df['img_path'] == file_path.split("/")[1]].values[0],
-                    # "LWL" : df['LWL'][df['img_path'] == file_path.split("/")[1]].values[0],
-                    # "Beam" : df['Beam'][df['img_path'] == file_path.split("/")[1]].values[0],
-                    # "S.A. (reported)" : df['S.A. (reported)'][df['img_path'] == file_path.split("/")[1]].values[0],
-                    # "Draft (max)" : df['Draft (max)'][df['img_path'] == file_path.split("/")[1]].values[0],
-                    # "Displacement" : df['Displacement'][df['img_path'] == file_path.split("/")[1]].values[0],
-                    # "Ballast" : df['Ballast'][df['img_path'] == file_path.split("/")[1]].values[0],
-                    # "S.A./Disp." : df['S.A./Disp.'][df['img_path'] == file_path.split("/")[1]].values[0],
-                    # "Bal./Disp." : df['Bal./Disp.'][df['img_path'] == file_path.split("/")[1]].values[0],
-                    # "Disp./Len." : df['Disp./Len.'][df['img_path'] == file_path.split("/")[1]].values[0],
-                    "Construction" : df['Construction'][df['img_path'] == file_path.split("/")[1]].values[0],
-                    "Ballast Type" : df['Ballast Type'][df['img_path'] == file_path.split("/")[1]].values[0],
-                    # "First Built" : df['First Built'][df['img_path'] == file_path.split("/")[1]].values[0],
-                    # "# Built" : df['# Built'][df['img_path'] == file_path.split("/")[1]].values[0],
-                    "Designer" : df['Designer'][df['img_path'] == file_path.split("/")[1]].values[0],
-                    # "Comfort Ratio" : df['Comfort Ratio'][df['img_path'] == file_path.split("/")[1]].values[0],
-                    # "Capsize Screening Formula" : df['Capsize Screening Formula'][df['img_path'] == file_path.split("/")[1]].values[0],
-                    # "S#" : df['S#'][df['img_path'] == file_path.split("/")[1]].values[0],
-                    # "I" : df['I'][df['img_path'] == file_path.split("/")[1]].values[0],
-                    # "J" : df['J'][df['img_path'] == file_path.split("/")[1]].values[0],
-                    # "P" : df['P'][df['img_path'] == file_path.split("/")[1]].values[0],
-                    # "E" : df['E'][df['img_path'] == file_path.split("/")[1]].values[0],
-                    # "SPL/TPS" : df['SPL/TPS'][df['img_path'] == file_path.split("/")[1]].values[0],
-                    # "ISP" : df['ISP'][df['img_path'] == file_path.split("/")[1]].values[0],
-                    # "S.A. Fore" : df['S.A. Fore'][df['img_path'] == file_path.split("/")[1]].values[0],
-                    # "S.A. Main" : df['S.A. Main'][df['img_path'] == file_path.split("/")[1]].values[0],
-                    # "S.A./Disp. (calc.)" : df['S.A./Disp. (calc.)'][df['img_path'] == file_path.split("/")[1]].values[0],
-                    # "Est. Forestay Len." : df['Est. Forestay Len.'][df['img_path'] == file_path.split("/")[1]].values[0],
-                    "name" : df['name'][df['img_path'] == file_path.split("/")[1]].values[0],
-                    # "Draft (min)" : df['Draft (min)'][df['img_path'] == file_path.split("/")[1]].values[0],
-                    # "Builder" : df['Builder'][df['img_path'] == file_path.split("/")[1]].values[0], 
-                    # "Model" : df['Model'][df['img_path'] == file_path.split("/")[1]].values[0],
-                    # "Last Built" : df['Last Built'][df['img_path'] == file_path.split("/")[1]].values[0],
-                    # "Make" : df['Make'][df['img_path'] == file_path.split("/")[1]].values[0],
-                    # "Type" : df['Type'][df['img_path'] == file_path.split("/")[1]].values[0],
-                    # "HP" : df['HP'][df['img_path'] == file_path.split("/")[1]].values[0],
-                    # "Fuel" : df['Fuel'][df['img_path'] == file_path.split("/")[1]].values[0],
-                    # "Headroom" : df['Headroom'][df['img_path'] == file_path.split("/")[1]].values[0],
-                    # "Water" : df['Water'][df['img_path'] == file_path.split("/")[1]].values[0],
-                    # "Website" : df['Website'][df['img_path'] == file_path.split("/")[1]].values[0],
-                    # "KSP" : df['KSP'][df['img_path'] == file_path.split("/")[1]].values[0],
-                    # "PY" : df['PY'][df['img_path'] == file_path.split("/")[1]].values[0],
-                    # "EY" : df['EY'][df['img_path'] == file_path.split("/")[1]].values[0],
-                    # "Bridgedeck Clearance" : df['Bridgedeck Clearance'][df['img_path'] == file_path.split("/")[1]].values[0],
-                    # "Mast Height from DWL" : df['Mast Height from DWL'][df['img_path'] == file_path.split("/")[1]].values[0],
-                    # "Bruce Number" : df['Bruce Number'][df['img_path'] == file_path.split("/")[1]].values[0],
+                idx = df.index[(df['img_path'] == file_path.split("/")[1])].values[0]
+                yield file_path , {
+                    "img_path"      : {"path": file_path, "bytes": file_obj.read()},
+                    "Hull Type"     : Hull_Type_Classes[df['Hull Type'][idx]],
+                    "Rigging Type"  : Rigging_Type_Classes[df['Rigging Type'][idx]],
+                    "Construction"  : Construction_Classes[df['Construction'][idx]],
+                    "Ballast Type"  : Ballast_Type_Classes[df['Ballast Type'][idx]],
+                    "Designer"      : Designer_Classes[df['Designer'][idx]],
+                    "name"          : Name_Classes[df['name'][idx]],
                 }
